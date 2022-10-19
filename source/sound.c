f3aadb4371a472575e21d0719f70171f9a0f79ae
pepsiman3 <adamthomascate@gmail.com>
2022-10-18 21:34:51 -0600

    roll audio updates into function for readability

--- source/sound.c
+++ source/sound.c
@@ -2,3 +2,12 @@
 
 xm64player_t xm;
 wav64_t wav;
+
+void update_audio(){
+    if (audio_can_write()) 
+    {    	
+        short *buf = audio_write_begin();
+        mixer_poll(buf, audio_get_buffer_length());
+        audio_write_end();
+    }
+}
